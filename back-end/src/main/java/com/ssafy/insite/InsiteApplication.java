2903b7da1f32c7c2200e75ce29b507749ddfeda5
PGJeong <pilgyo718@gmail.com>
2025-09-03 12:08:37 +0900

    [BE] init: Spring Boot 프로젝트 생성

---
+++ back-end/src/main/java/com/ssafy/insite/InsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.insite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsiteApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/ssafy/insite/InsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.insite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
