78bfe90742b976b424f103b0ab02b82319564fa2
dahyunsouth <dhnn1536@naver.com>
2025-09-06 19:50:22 +0900

    [FE] design: 로그인/회원가입 card에 drop-shadow 추가

--- front-end/src/components/atoms/Card/Authentication.tsx
+++ front-end/src/components/atoms/Card/Authentication.tsx
@@ -16,6 +16,9 @@ const AuthenticationCard = ({
         bg-[#FFFFFF]
         ${className}
       `}
+      style={{
+        filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))'
+      }}
       {...props}
     >
       {children}
