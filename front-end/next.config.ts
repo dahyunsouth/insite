95e20ce866389257a9132869a232d487019e2252
홍지훈 <wlgns9807@naver.com>
2025-09-09 22:35:40 +0900

    Edit next.config.ts

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   // 2025.09.09 홍지훈 인프라 배포할때 오류 땜 일단 추가해둔겁니당
+    eslint: { ignoreDuringBuilds: true }, // 린트 에러가 있어도 빌드 계속
   /* config options here */
 };
+  // 필요시 타입오류도 임시무시 (권장X)
+  // typescript: { ignoreBuildErrors: true },
 
 export default nextConfig;
