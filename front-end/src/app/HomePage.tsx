72980c334e0f568e6fec7f03a12acac3ee3ac224
dahyunsouth <dhnn1536@naver.com>
2025-09-14 10:11:05 +0900

    [FE] feat: '상권 비교' modal 구현 중 임시 저장

--- front-end/src/app/HomePage.tsx
+++ front-end/src/app/HomePage.tsx
@@ -157,7 +157,7 @@ export default function HomePage() {
       />
 
       {/* Compare modal: right-side overlay (covers right 75%) */}
-      <CompareTradeAreasModal open={isCompareOpen} onClose={() => setIsCompareOpen(false)} leftOpen={true} />
+      <CompareTradeAreasModal open={isCompareOpen} onClose={() => setIsCompareOpen(false)} leftOpen={false} />
 
       {/* 인증 모달 (AuthModalWrapper) - 조건부 렌더 */}
       {isAuthOpen && (
