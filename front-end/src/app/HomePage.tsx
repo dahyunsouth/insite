2676870b6fd3ffb2b5e4a860f5dd4e9b718eeaea
DESKTOP-RBNO6LS\SSAFY <phangmin03@naver.com>
2025-09-15 10:42:05 +0900

    [FE] feat: 지도

--- front-end/src/app/HomePage.tsx
+++ front-end/src/app/HomePage.tsx
@@ -137,8 +137,8 @@ export default function HomePage() {
       {/* Bottom-center CTA preview for verification */}
       <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-30">
         <CtaPillButton
-          label="강남역 상권 분석 자세히 보기"
-          ariaLabel="강남역 상권 분석 자세히 보기"
+          label="상권 분석 자세히 보기"
+          ariaLabel="상권 분석 자세히 보기"
           onPress={() => setIsDetailOpen(true)}
         />
       </div>
