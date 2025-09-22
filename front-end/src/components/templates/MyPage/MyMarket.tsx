ee97ec3245b5818dae32c976c0fa948ff1c56551
dahyunsouth <dhnn1536@naver.com>
2025-09-23 07:31:29 +0900

    [FE] feat: '저장된 상권 탭 - 체크박스' onClick, onChange 이벤트 중복 실행 문제 해결

--- front-end/src/components/templates/MyPage/MyMarket.tsx
+++ front-end/src/components/templates/MyPage/MyMarket.tsx
@@ -214,9 +214,6 @@ const MyMarket: React.FC<MyMarketProps> = ({
                  >
                    <CheckBox 
                      checked={isSelected}
-                     onChange={(checked) => {
-                       handleCheckboxChange(area.trdarCd, checked);
-                     }}
                    />
                  </div>
                  <div className='flex flex-col gap-2 w-full'>
